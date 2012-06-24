981b9c7b5c1b2c7e5350b7b4425ca732b410e2f0
Nicolas Blanchot <nicolasblanchot@gmail.com>
2012-06-25 00:38:54 +0200

    modify port for production

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ app.get('/activities', api.activities);
 app.get('/details', api.details);
 app.get('/*', routes.error404);
 
-app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
