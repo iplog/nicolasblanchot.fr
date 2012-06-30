b9e80d14dee68c858616549ef5c723d8208570ce
Nicolas Blanchot <nicolasblanchot@gmail.com>
2012-06-30 23:19:59 +0200

    display app in iphone

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,26 @@
-
-/*
+/**
  * GET home page.
  */
-exports.index = function(req, res){
+exports.index = function(req, res) {
   var data = {
-    locals: {
-      title: 'Nicolas Blanchot Resume'
+    locals : {
+      title : 'Nicolas Blanchot Resume'
     }
+
   };
+  if (!req.query['no_check']) {
+    data.layout = 'demo_layout';
+  }
   res.render('index', data);
 };
-/*
+/**
  * send 404 error
  */
 exports.error404 = function(req, res) {
   var path = req.params[0];
   var data = {
-    locals: {
-      title: path +' - Error404' ,
+    locals : {
+      title : path +' - Error404' ,
       host : req.headers.host,
       path : req.params[0]
     },
@@ -25,12 +28,13 @@ exports.error404 = function(req, res) {
   };
   res.render('error404', data);
 };
-
+/**
+ * send under consturction page
+ */
 exports.underConstruction = function(req, res) {
-  var path = req.params[0];
   var data = {
-    locals: {
-      title: 'underConstruct'
+    locals : {
+      title : 'underConstruct'
     },
     layout : 'error_layout'
   };
