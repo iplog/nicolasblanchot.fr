c2fcbfb4080e10eb95efd81485ec0a08266c1b7d
Nicolas Blanchot <nicolasblanchot@gmail.com>
2012-07-15 16:08:10 +0200

    create some very basic tests on collections using factory models

---
+++ public/tests/spec/collections_spec.js
@@ -0,0 +1,110 @@
+define([
+  'collections/categories',
+  'collections/activities',
+  'collections/details',
+  'factory/category',
+  'factory/activity',
+  'factory/detail'
+], function(Categories, Activities, Details) {
+  describe('Tests categories collection', function() {
+    var collection;
+
+    beforeEach(function() {
+      collection = new Categories();
+    });
+
+    it('Can add Model instances as objects and arrays.', function() {
+        expect(collection.length).toBe(0);
+
+        var model = Factory.build('category', {
+          name : 'plop'
+        });
+
+        // it's not neccessary to create a complete model instance. Collection
+        // is gonna do it for us :)
+        collection.add(model);
+
+        // how many categories have been added so far?
+        expect(collection.length).toBe(1);
+
+        var arr = [];
+        arr.push(Factory.build('category', {
+          name : 'Blah'
+        }));
+        arr.push(Factory.build('category', {
+          name : 'Bleh'
+        }));
+
+        collection.add(arr);
+
+        // how many are there in total now?
+        expect(collection.length).toBe(3);
+    });
+  });
+
+  describe('Tests activities collection', function() {
+    var collection;
+
+    beforeEach(function() {
+      collection = new Activities();
+    });
+
+    it('Can add Model instances as objects and arrays.', function() {
+        expect(collection.length).toBe(0);
+
+        var model = Factory.build('activity');
+
+        // it's not neccessary to create a complete model instance. Collection
+        // is gonna do it for us :)
+        collection.add(model);
+
+        // how many categories have been added so far?
+        expect(collection.length).toBe(1);
+
+        var arr = [];
+        arr.push(Factory.build('activity', {
+          title : 'argh'
+        }));
+        arr.push(Factory.build('activity', {
+          title : 'brrr'
+        }));
+
+        collection.add(arr);
+
+        // how many are there in total now?
+        expect(collection.length).toBe(3);
+    });
+  });
+
+
+  describe('Tests details collection', function() {
+    var collection;
+
+    beforeEach(function() {
+      collection = new Details();
+    });
+
+    it('Can add Model instances as objects and arrays.', function() {
+        expect(collection.length).toBe(0);
+
+        var model = Factory.build('detail');
+
+        // it's not neccessary to create a complete model instance. Collection
+        // is gonna do it for us :)
+        collection.add(model);
+
+        // how many categories have been added so far?
+        expect(collection.length).toBe(1);
+
+        var arr = [];
+        arr.push(Factory.build('detail'));
+        arr.push(Factory.build('detail'));
+        arr.push(Factory.build('detail'));
+
+        collection.add(arr);
+
+        // how many are there in total now?
+        expect(collection.length).toBe(4);
+    });
+  });
+});
